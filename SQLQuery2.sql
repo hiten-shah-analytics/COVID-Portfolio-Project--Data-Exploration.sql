10559fa75d69b84676852593602722f9ed438dc4
Hiten Shah <hiten1919@gmail.com>
2026-06-15 00:25:24 +0530

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,156 +1,90 @@
-USE PortfolioProject;
-GO
+/*
+Covid 19 Data Exploration 
 
-DROP TABLE IF EXISTS CovidDeaths;
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
-CREATE TABLE CovidDeaths (
-    iso_code VARCHAR(10),
-    continent VARCHAR(50),
-    [location] VARCHAR(100),
-    date DATE,
-    population BIGINT,
-    total_cases FLOAT,
-    new_cases FLOAT,
-    new_cases_smoothed FLOAT,
-    total_deaths FLOAT,
-    new_deaths FLOAT,
-    new_deaths_smoothed FLOAT,
-    total_cases_per_million FLOAT,
-    new_cases_per_million FLOAT,
-    new_cases_smoothed_per_million FLOAT,
-    total_deaths_per_million FLOAT,
-    new_deaths_per_million FLOAT,
-    new_deaths_smoothed_per_million FLOAT,
-    reproduction_rate FLOAT,
-    icu_patients FLOAT,
-    icu_patients_per_million FLOAT,
-    hosp_patients FLOAT,
-    hosp_patients_per_million FLOAT,
-    weekly_icu_admissions FLOAT,
-    weekly_icu_admissions_per_million FLOAT,
-    weekly_hosp_admissions FLOAT,
-    weekly_hosp_admissions_per_million FLOAT
-);
-GO
-CREATE TABLE CovidVaccinations (
-    iso_code VARCHAR(10),
-    continent VARCHAR(50),
-    location VARCHAR(100),
-    date DATE,
-    total_tests FLOAT,
-    new_tests FLOAT,
-    total_tests_per_thousand FLOAT,
-    new_tests_per_thousand FLOAT,
-    new_tests_smoothed FLOAT,
-    new_tests_smoothed_per_thousand FLOAT,
-    positive_rate FLOAT,
-    tests_per_case FLOAT,
-    tests_units VARCHAR(50),
-    total_vaccinations FLOAT,
-    people_vaccinated FLOAT,
-    people_fully_vaccinated FLOAT,
-    new_vaccinations FLOAT,
-    new_vaccinations_smoothed FLOAT,
-    total_vaccinations_per_hundred FLOAT,
-    people_vaccinated_per_hundred FLOAT,
-    people_fully_vaccinated_per_hundred FLOAT,
-    new_vaccinations_smoothed_per_million FLOAT,
-    stringency_index FLOAT,
-    population_density FLOAT,
-    median_age FLOAT,
-    aged_65_older FLOAT,
-    aged_70_older FLOAT,
-    gdp_per_capita FLOAT,
-    extreme_poverty FLOAT,
-    cardiovasc_death_rate FLOAT,
-    diabetes_prevalence FLOAT,
-    female_smokers FLOAT,
-    male_smokers FLOAT,
-    handwashing_facilities FLOAT,
-    hospital_beds_per_thousand FLOAT,
-    life_expectancy FLOAT,
-    human_development_index FLOAT
-);
-GO
--- Import CovidDeaths
-BULK INSERT CovidDeaths
-FROM 'C:\Users\Hiten Shah\Downloads\CovidDeaths.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n',
-    TABLOCK
-);
-GO
+*/
 
--- Import CovidVaccinations
-BULK INSERT CovidVaccinations
-FROM 'C:\Users\Hiten Shah\Downloads\CovidVaccinations.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n',
-    TABLOCK
-);
-GO
--- Import CovidDeaths
-BULK INSERT CovidDeaths
-FROM 'C:\Users\Hiten Shah\Downloads\CovidDeaths.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n',
-    TABLOCK
-);
-GO
-
--- Import CovidVaccinations
-BULK INSERT CovidVaccinations
-FROM 'C:\Users\Hiten Shah\Downloads\CovidVaccinations.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n',
-    TABLOCK
-);
-GO
-SELECT *
-FROM PortfolioProject..CovidDeaths
-Where continent is not null
+Select *
+From PortfolioProject..CovidDeaths
+Where continent is not null 
 order by 3,4
-SELECT *
-FROM PortfolioProject..CovidVaccinations
-order by 3,4
+
+
+-- Select Data that we are going to be starting with
+
 Select Location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths
-Where continent is not null
+Where continent is not null 
 order by 1,2
-Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+
+
+-- Total Cases vs Total Deaths
+-- Shows likelihood of dying if you contract covid in your country
+
+Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
 Where location like '%states%'
+and continent is not null 
 order by 1,2
-Select Location, date, population, total_cases, total_deaths, (total_cases/population)*100 as PercentagePopulationInfected
+
+
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid
+
+Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
-Where location like '%states%'
+--Where location like '%states%'
 order by 1,2
+
+
+-- Countries with Highest Infection Rate compared to Population
+
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
+--Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
+
+
+-- Countries with Highest Death Count per Population
+
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
+--Where location like '%states%'
 Where continent is not null 
 Group by Location
 order by TotalDeathCount desc
-Select location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+
+
+
+-- BREAKING THINGS DOWN BY CONTINENT
+
+-- Showing contintents with the highest death count per population
+
+Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
-Where continent is null 
-Group by location
+--Where location like '%states%'
+Where continent is not null 
+Group by continent
 order by TotalDeathCount desc
+
+
+
+-- GLOBAL NUMBERS
+
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
+--Where location like '%states%'
 where continent is not null 
+--Group By date
 order by 1,2
+
+
+
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
+
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
@@ -160,6 +94,10 @@ Join PortfolioProject..CovidVaccinations vac
 	and dea.date = vac.date
 where dea.continent is not null 
 order by 2,3
+
+
+-- Using CTE to perform Calculation on Partition By in previous query
+
 With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
 as
 (
@@ -171,9 +109,15 @@ Join PortfolioProject..CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
+--order by 2,3
 )
 Select *, (RollingPeopleVaccinated/Population)*100
 From PopvsVac
+
+
+
+-- Using Temp Table to perform Calculation on Partition By in previous query
+
 DROP Table if exists #PercentPopulationVaccinated
 Create Table #PercentPopulationVaccinated
 (
@@ -198,6 +142,12 @@ Join PortfolioProject..CovidVaccinations vac
 
 Select *, (RollingPeopleVaccinated/Population)*100
 From #PercentPopulationVaccinated
+
+
+
+
+-- Creating View to store data for later visualizations
+
 Create View PercentPopulationVaccinated as
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
